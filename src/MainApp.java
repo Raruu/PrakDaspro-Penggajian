622ce6c28b6d219702038f63195514b880b9eea3
Raruu Kagami <108914099+Raruu@users.noreply.github.com>
2023-09-13 11:13:51 +0700

    Add: Project structure

---
+++ src/MainApp.java
@@ -0,0 +1,9 @@
+public class MainApp {
+    public static void main(String[] args) throws Exception {
+        System.out.println("####################################");
+        System.out.println("#                                  #");
+        System.out.println("#         Sistem Penggajian        #");
+        System.out.println("#                                  #");
+        System.out.println("####################################");
+    }
+}
