82ec43747fcd4d79890ee580068022b228349750
Raruu Kagami <108914099+Raruu@users.noreply.github.com>
2023-10-17 09:33:51 +0700

    update slip,add comment,add exit in login,fix typo

--- src/MainApp.java
+++ src/MainApp.java
@@ -3,6 +3,7 @@ import java.util.Scanner;
 public class MainApp {
     public static void main(String[] args) throws Exception {
         final String PagarPemisah = "###".repeat(47); 
+        final Scanner scInput = new Scanner(System.in);        
         System.out.println(
                 "############################################################################################################################################\n" +
                 "############################################################################################################################################\n" +
@@ -15,8 +16,7 @@ public class MainApp {
                 "############################################################################################################################################\n" +
                 "############################################################################################################################################\n" +
                 "                                                                                                                                    ");
-
-        Scanner scInput = new Scanner(System.in);
+       
         // untuk variable int yang butuh input, taruh sini
         int jmlMasuk, jamLembur;
         char golongan;     
@@ -28,31 +28,37 @@ public class MainApp {
         Boolean dataValid = false, transfStatus = false, login = false;
         // biar apa? biar kelihatan rapi   
 
-    while(login == false) 
-    {
-        System.out.println("------LOGIN------");
-        System.out.println("\nUsername: ");
-        username = scInput.nextLine();
-        System.out.println("\nPassword: ");
-        password = scInput.nextLine();
-
-        if (username.equals("admin") && password.equals("123456")) {
-            login = true;
-            System.out.println("\n------LOGIN BERHASIL------");
-        }
-        else
+        // Login
+        while(login == false) 
         {
-            System.out.println("\nUsername atau Password anda salah!");
-        }
+            System.out.println("------LOGIN------");
+            System.out.println("Tekan CTRL+C atau isi \"0\" pada Username untuk keluar program");
+            System.out.print("\nUsername: ");
+            username = scInput.nextLine();
+            if (username.equals("0")) {
+                scInput.close();
+                return;
+            }
+            System.out.print("\nPassword: ");
+            password = scInput.nextLine();
 
-    }
+            if (username.equals("admin") && password.equals("123456")) {
+                login = true;
+                System.out.println("\n------LOGIN BERHASIL------");
+            }
+            else
+            {
+                System.out.println("\nUsername atau Password anda salah!");
+            }
+        }
         
         Boolean isRunning = true;
         int menuItem;
         do {
-            System.out.println("\nMain Menu: ");
-            System.out.println("0. Keluar Program\n1. Perhitungan Gaji");
-            System.out.print("Pilih menu: ");
+            // Main Menu
+            System.out.println("\nMain Menu ["+ username +"]: ");
+            System.out.println("1. Perhitungan Gaji\n0. Keluar Program");
+            System.out.print("\n(int) Pilih menu: ");
             menuItem = scInput.nextInt();
             scInput.nextLine();
             switch (menuItem) {
@@ -60,15 +66,16 @@ public class MainApp {
                     isRunning = false;
                     break;
                 case 1:
-                    System.out.print("Masukkan Name\t\t\t\t\t: ");
+                    System.out.print("Masukkan Nama\t\t\t\t\t: ");
                     String nama = scInput.nextLine();
                     System.out.print("(char) Masukkan Golongan\t\t\t: ");
                     golongan = Character.toUpperCase(scInput.next().charAt(0));
                     System.out.print("(bilangan real) Masukkan Jumlah Masuk\t\t: ");
                     jmlMasuk = scInput.nextInt();
                     System.out.print("(bilangan real) Masukkan Total Jam Lembur\t: ");
-                    jamLembur = scInput.nextInt();                    
+                    jamLembur = scInput.nextInt();          
 
+                    // Perhitungan Gaji
                     switch (golongan) {
                         case 'A':
                             pokok_GajiHarian = 160000;
@@ -98,21 +105,38 @@ public class MainApp {
                             System.out.println("Golongan: " + golongan
                                     + " tidak ada, melanjutkan perhitungan dengan data seadanya");
                     }
+                    long pokok_Gaji = jmlMasuk * pokok_GajiHarian;
+                    int bonus_Gaji = jamLembur * bonus;
+                    total_Gaji = pokok_Gaji + bonus_Gaji + uangTunjangan;
+                    double potongan_Pajak = total_Gaji * pajak;
+                    hasil_akhir = total_Gaji - potongan_Pajak;
 
-                    total_Gaji = (jmlMasuk * pokok_GajiHarian) + (jamLembur * bonus) + uangTunjangan;
-                    hasil_akhir = total_Gaji - (total_Gaji * pajak);
+                    // Slip Gaji
                     System.out.println(PagarPemisah+"\n");
-                    System.out.println("Atas nama: " + nama);
-                    System.out.println("Total Gaji\t\t\t\t\t: Rp." + (int) hasil_akhir);
-                    System.out.println(PagarPemisah+"\n");
+                    System.out.println("Slip Gaji\nAtas nama: " + nama);
+                    System.out.println(
+                        "\nPenghasilan: " +
+                        "\nGolongan\t\t\t\t\t: " + golongan +
+                        "\nGaji Pokok\t\t\t\t\t: Rp." + pokok_Gaji +
+                        "\nBonus Gaji\t\t\t\t\t: Rp." + bonus_Gaji +
+                        "\nTunjangan\t\t\t\t\t: Rp." + uangTunjangan +
+                        "\n--------------------------------------------------------------------------" +
+                        "\nTotal Penghasilan\t\t\t\t: Rp." + (long) total_Gaji);
+                    System.out.println(
+                        "\nPotongan: " +
+                        "\nPajak\t\t\t\t\t\t: Rp." + (long) potongan_Pajak);
+                    System.out.println(
+                        "\n--------------------------------------------------------------------------" +
+                        "\nTotal Gaji Bersih\t\t\t\t: Rp." + (long) hasil_akhir);
 
+                    // Pembayaran / Transfer
+                    System.out.println(PagarPemisah+"\n");
                     dataValid = namaBank.equalsIgnoreCase("BRI") && noRekening.length() == 12;
                     if (dataValid) {
                         System.out.println(
-                                "Memulai transfer uang sebesar Rp." + (int) hasil_akhir + " ke " + noRekening + "("
-                                        + namaBank + ")");
-                        transfStatus = true;
-                        System.out.println(PagarPemisah+"\n");
+                                "Memulai transfer uang sebesar Rp." + (int) hasil_akhir + 
+                                " ke " + noRekening + "(" + namaBank + ")");
+                        transfStatus = true;                        
                         if (transfStatus) {
                             System.out.println("Transfer Berhasil");
                         } else {
@@ -125,6 +149,7 @@ public class MainApp {
                         System.out.println("Nama Bank\t: " + namaBank);
                         System.out.println("No Rekening\t: " + noRekening);
                     }
+                    System.out.println(PagarPemisah+"\n");
                     System.out.println("Tugas selesai, Kembali ke menu. . .");
                     break;
 
